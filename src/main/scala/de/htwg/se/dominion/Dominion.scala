0ecb6bc00ee040819ac7f035670ad4065a250308
Luis Nothvogel <L_noth@yahoo.de>
2019-03-28 17:34:20 +0100

    Deleted bickdig variable and edited the name string

--- src/main/scala/de/htwg/se/dominion/Dominion.scala
+++ src/main/scala/de/htwg/se/dominion/Dominion.scala
@@ -1,9 +1,8 @@
 package de.htwg.se.dominion
 
 object dominion {
-  var name = "Dominion!"
+  var name = "Dominion"
   var creator = "Luis Nothvogel & Luca Stratmann"
-  var bickdig = "Test"
 
   def main(args: Array[String]): Unit = {
     println(name + " wurde von " + creator + " erstellt")
