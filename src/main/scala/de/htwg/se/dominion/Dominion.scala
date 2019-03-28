018b3d4067e9d357fac712b8cffca86e1d2a238f
Luca Stratmann <luca.stratmannmx-topmail.de>
2019-03-28 17:30:08 +0100

    Dies Test

--- src/main/scala/de/htwg/se/dominion/Dominion.scala
+++ src/main/scala/de/htwg/se/dominion/Dominion.scala
@@ -3,6 +3,7 @@ package de.htwg.se.dominion
 object dominion {
   var name = "Dominion!"
   var creator = "Luis Nothvogel & Luca Stratmann"
+  var bickdig = "Test"
 
   def main(args: Array[String]): Unit = {
     println(name + " wurde von " + creator + " erstellt")
