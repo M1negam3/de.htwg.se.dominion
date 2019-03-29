edb27fcfb1b6411b786c7739a49da9983b701c37
M1negam3 <luca.stratmann@gmx-topmail.de>
2019-03-29 11:24:52 +0100

    Testing Display of Username

--- src/main/scala/de/htwg/se/dominion/Dominion.scala
+++ src/main/scala/de/htwg/se/dominion/Dominion.scala
@@ -3,6 +3,7 @@ package de.htwg.se.dominion
 object dominion {
   var name = "Dominion"
   var creator = "Luis Nothvogel & Luca Stratmann"
+  var test ="Username testen"
 
   def main(args: Array[String]): Unit = {
     println(name + " wurde von " + creator + " erstellt")
