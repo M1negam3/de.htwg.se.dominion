148209cc054b43fdc74f88ccd1851d450e98a3c7
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2019-03-29 11:46:53 +0100

    Testing implemented Branch

--- src/main/scala/de/htwg/se/dominion/Dominion.scala
+++ src/main/scala/de/htwg/se/dominion/Dominion.scala
@@ -3,6 +3,7 @@ package de.htwg.se.dominion
 object dominion {
   var name = "Dominion"
   var creator = "Luis Nothvogel & Luca Stratmann"
+  var testBranch = "testing new implemented Branch"
 
   def main(args: Array[String]): Unit = {
     println(name + " wurde von " + creator + " erstellt")
