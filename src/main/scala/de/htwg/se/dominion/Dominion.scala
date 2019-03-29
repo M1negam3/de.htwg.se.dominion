9685becf63ae988d8e68b925c7d734a9349d91af
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2019-03-29 11:28:11 +0100

    Removing testing Display of Username

--- src/main/scala/de/htwg/se/dominion/Dominion.scala
+++ src/main/scala/de/htwg/se/dominion/Dominion.scala
@@ -3,7 +3,6 @@ package de.htwg.se.dominion
 object dominion {
   var name = "Dominion"
   var creator = "Luis Nothvogel & Luca Stratmann"
-  var test ="Username testen"
 
   def main(args: Array[String]): Unit = {
     println(name + " wurde von " + creator + " erstellt")
