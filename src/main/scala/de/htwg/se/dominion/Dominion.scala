bc32cfc1998b9860e71bae45fedba93b1c5db244
Luis Nothvogel <L_noth@yahoo.de>
2019-03-29 11:32:02 +0100

    changed name

--- src/main/scala/de/htwg/se/dominion/Dominion.scala
+++ src/main/scala/de/htwg/se/dominion/Dominion.scala
@@ -2,7 +2,7 @@ package de.htwg.se.dominion
 
 object dominion {
   var name = "Dominion"
-  var creator = "Luis Nothvogel & Luca Stratmann"
+  var creator = "Luis Big Dick Nothvogel & Luca Stratmann"
 
   def main(args: Array[String]): Unit = {
     println(name + " wurde von " + creator + " erstellt")
