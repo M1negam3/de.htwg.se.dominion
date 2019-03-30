9f79b0b67fa4d238db2cb62eafe5ae76fdf2e427
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2019-03-30 17:31:29 +0100

    First two tests passed, but had no value, were just for testing how testing works

--- src/main/scala/de/htwg/se/dominion/Tests.scala
+++ src/main/scala/de/htwg/se/dominion/Tests.scala
@@ -1,17 +1,16 @@
 package de.htwg.se.dominion
-import org.scalactic._
+import de.htwg.se.dominion.Gamevariables._
 import org.scalatest._
-import Gamevariables._
 
 class Tests extends WordSpec with Matchers {
   "A Card" when {
     "not set to any value" should {
       val emptyCard = Value(0)
       "have a value 0" in {
-        emptyCard.value should be (0)
+        emptyCard.charAt(0) should be ('0')
       }
       "not be set" in {
-        emptyCard.isSet should be (false)
+        emptyCard.isEmpty should be (false)
       }
     }
   }
