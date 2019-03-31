148f26bd6b0107e6367c2b276078b4ba2a2ff75f
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2019-03-31 12:24:56 +0200

    Adding various gamevariables to implement later card setups

--- src/main/scala/de/htwg/se/dominion/Gamevariables.scala
+++ src/main/scala/de/htwg/se/dominion/Gamevariables.scala
@@ -1,7 +1,13 @@
 package de.htwg.se.dominion
 
 object Gamevariables {
-  var BuyValue = Array ("0", "1", "2", "3", "4")
+  var BuyValue = Array ("0", "1", "2", "3", "4", "5","6","8")
+  var MoneyValue = Array("1","2","3")
+  var WpValue = Array("1", "3", "6")
+  var ActionValue = Array ("1", "2")
+  var BuyAdditionValue = Array ("1", "2")
+  var BonusMoneyValue = Array ("1", "2")
+  var EffektValue = Array ("Still to add")
   var Value = Array("0", "1", "2", "3")
   var Effect = Array ("Action", "Buy", "Card")
   var Hand = new Array[String](5)
