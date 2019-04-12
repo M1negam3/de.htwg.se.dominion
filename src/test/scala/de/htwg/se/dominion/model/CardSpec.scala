3197e9883f71a0202d0d7cbfe1598b1f7139c0c7
Luis Nothvogel <L_noth@yahoo.de>
2019-04-12 09:57:21 +0200

    added a test for card

--- src/test/scala/de/htwg/se/dominion/model/CardSpec.scala
+++ src/test/scala/de/htwg/se/dominion/model/CardSpec.scala
@@ -5,33 +5,33 @@ import org.scalatest._
 class CardSpec extends WordSpec with Matchers {
   "A Card" when {
     "new" should {
-      val MoneyCard = Card(0, 1, 0, 0, 0, 0, 0, "None", "Copper")
+      val copper = Card (0, 1, 0, 0, 0, 0, 0, "None", "Copper")
       "have a BuyValue" in {
-          MoneyCard.BuyValue should be(0)
+        copper.BuyValue should be(0)
       }
       "have a MoneyValue" in {
-        MoneyCard.MoneyValue should be(1)
+        copper.MoneyValue should be(1)
       }
       "have a WpValue" in {
-        MoneyCard.WpValue should be(0)
+        copper.WpValue should be(0)
       }
       "have a ActionValue" in {
-        MoneyCard.ActionValue should be(0)
+        copper.ActionValue should be(0)
       }
       "have a BuyadditionValue" in {
-        MoneyCard.BuyadditionValue should be(0)
+        copper.BuyadditionValue should be(0)
       }
       "have a BonusMoneyValue" in {
-        MoneyCard.BonusMoneyValue should be(0)
+        copper.BonusMoneyValue should be(0)
       }
       "have a DrawingValue" in {
-        MoneyCard.DrawingValue should be(0)
+        copper.DrawingValue should be(0)
       }
       "have a EffectValue" in {
-        MoneyCard.EffectValue should be("None")
+        copper.EffectValue should be("None")
       }
       "have a CardName" in {
-        MoneyCard.CardName should be("Copper")
+        copper.CardName should be("Copper")
       }
     }
   }
