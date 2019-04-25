e912843ba79e0287b9035c496a7f3fe0eaf52ee9
Luis Nothvogel <L_noth@yahoo.de>
2019-04-25 19:55:24 +0200

    finished deck and hand card creation + i/o

--- src/main/scala/de/htwg/se/dominion/model/GameStart.scala
+++ src/main/scala/de/htwg/se/dominion/model/GameStart.scala
@@ -10,48 +10,81 @@ object GameStart {
     val basicDeck : List[BasicCards] = List(BasicCards.copper, BasicCards.copper, BasicCards.copper, BasicCards.copper,
       BasicCards.copper, BasicCards.copper, BasicCards.mansion, BasicCards.mansion, BasicCards.mansion)
 
-    val player1Deck : List[BasicCards] = basicDeck
-    val player2Deck : List[BasicCards] = basicDeck
-    val player3Deck : List[BasicCards] = basicDeck
-    val player4Deck : List[BasicCards] = basicDeck
-    val player5Deck : List[BasicCards] = basicDeck
+    val player1Deck : List[BasicCards] = random.shuffle(basicDeck)
+    val player2Deck : List[BasicCards] = random.shuffle(basicDeck)
+    val player3Deck : List[BasicCards] = random.shuffle(basicDeck)
+    val player4Deck : List[BasicCards] = random.shuffle(basicDeck)
+    val player5Deck : List[BasicCards] = random.shuffle(basicDeck)
 
     if (pAmount == 2) {
       InputOutput.deckCreation(1)
-      val player1Hand : List[BasicCards] = random.shuffle(player1Deck)
       InputOutput.deckCreation(2)
-      val player2Hand : List[BasicCards] = random.shuffle(player2Deck)
 
-      for (x <- 0 until 5) {
-        val player1HandCards : List[BasicCards] = List(player1Hand(x))
-        InputOutput.HandCardCreation(player1HandCards, 1)
-        val player2HandCards : List[BasicCards] = List(player2Hand(x))
-        InputOutput.HandCardCreation(player2HandCards, 2)
-      }
+      val player1HandCards : List[BasicCards] = List(player1Deck.head, player1Deck(1), player1Deck(2), player1Deck(3),
+        player1Deck(4))
+      val player2HandCards : List[BasicCards] = List(player2Deck.head, player2Deck(1), player2Deck(2), player2Deck(3),
+        player2Deck(4))
+
+      InputOutput.HandCardCreation(player1HandCards, 1)
+      InputOutput.HandCardCreation(player2HandCards, 2)
     } else if (pAmount == 3) {
+      InputOutput.deckCreation(1)
+      InputOutput.deckCreation(2)
       InputOutput.deckCreation(3)
-      val player3Hand : List[BasicCards] = random.shuffle(player3Deck)
 
-      for (x <- 0 until 5) {
-        val player3HandCards: List[BasicCards] = List(player3Hand(x))
-        InputOutput.HandCardCreation(player3HandCards, 3)
-      }
+      val player1HandCards : List[BasicCards] = List(player1Deck.head, player1Deck(1), player1Deck(2), player1Deck(3),
+        player1Deck(4))
+      val player2HandCards : List[BasicCards] = List(player2Deck.head, player2Deck(1), player2Deck(2), player2Deck(3),
+        player2Deck(4))
+      val player3HandCards: List[BasicCards] = List(player3Deck.head, player3Deck(1), player3Deck(2), player3Deck(3),
+        player3Deck(4))
+
+      InputOutput.HandCardCreation(player1HandCards, 1)
+      InputOutput.HandCardCreation(player2HandCards, 2)
+      InputOutput.HandCardCreation(player3HandCards, 3)
     } else if (pAmount == 4) {
+      InputOutput.deckCreation(1)
+      InputOutput.deckCreation(2)
+      InputOutput.deckCreation(3)
       InputOutput.deckCreation(4)
-      val player4Hand : List[BasicCards] = random.shuffle(player4Deck)
 
-      for (x <- 0 until 5) {
-        val player4HandCards: List[BasicCards] = List(player4Hand(x))
-        InputOutput.HandCardCreation(player4HandCards, 4)
-      }
+      val player1HandCards : List[BasicCards] = List(player1Deck.head, player1Deck(1), player1Deck(2), player1Deck(3),
+        player1Deck(4))
+      val player2HandCards : List[BasicCards] = List(player2Deck.head, player2Deck(1), player2Deck(2), player2Deck(3),
+        player2Deck(4))
+      val player3HandCards: List[BasicCards] = List(player3Deck.head, player3Deck(1), player3Deck(2), player3Deck(3),
+        player3Deck(4))
+      val player4HandCards: List[BasicCards] = List(player4Deck.head, player4Deck(1), player4Deck(2), player4Deck(3),
+        player4Deck(4))
+
+      InputOutput.HandCardCreation(player1HandCards, 1)
+      InputOutput.HandCardCreation(player2HandCards, 2)
+      InputOutput.HandCardCreation(player3HandCards, 3)
+      InputOutput.HandCardCreation(player4HandCards, 4)
     } else if (pAmount == 5) {
+      InputOutput.deckCreation(1)
+      InputOutput.deckCreation(2)
+      InputOutput.deckCreation(3)
+      InputOutput.deckCreation(4)
       InputOutput.deckCreation(5)
-      val player5Hand : List[BasicCards] = random.shuffle(player5Deck)
 
-      for (x <- 0 until 5) {
-        val player5HandCards: List[BasicCards] = List(player5Hand(x))
-        InputOutput.HandCardCreation(player5HandCards, 5)
-      }
+      val player1HandCards : List[BasicCards] = List(player1Deck.head, player1Deck(1), player1Deck(2), player1Deck(3),
+        player1Deck(4))
+      val player2HandCards : List[BasicCards] = List(player2Deck.head, player2Deck(1), player2Deck(2), player2Deck(3),
+        player2Deck(4))
+      val player3HandCards: List[BasicCards] = List(player3Deck.head, player3Deck(1), player3Deck(2), player3Deck(3),
+        player3Deck(4))
+      val player4HandCards: List[BasicCards] = List(player4Deck.head, player4Deck(1), player4Deck(2), player4Deck(3),
+        player4Deck(4))
+      val player5HandCards: List[BasicCards] = List(player5Deck.head, player5Deck(1), player5Deck(2), player5Deck(3),
+        player5Deck(4))
+
+      InputOutput.HandCardCreation(player1HandCards, 1)
+      InputOutput.HandCardCreation(player2HandCards, 2)
+      InputOutput.HandCardCreation(player3HandCards, 3)
+      InputOutput.HandCardCreation(player4HandCards, 4)
+      InputOutput.HandCardCreation(player5HandCards, 5)
+
     }
   }
 }
