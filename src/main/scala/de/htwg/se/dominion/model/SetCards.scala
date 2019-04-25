e114fa263d007c56ea6a29013ae48c52efa94fa0
Luca Stratmann <luca.stratmannmx-topmail.de>
2019-04-25 21:25:28 +0200

    Deleted useless Test and Class

--- src/main/scala/de/htwg/se/dominion/model/SetCards.scala
+++
@@ -1,8 +0,0 @@
-package de.htwg.se.dominion.model
-
-case class SetCards(CostValue : Int, MoneyValue : Int, WpValue : Int, ActionValue : Int, BuyAdditionValue : Int,
-                BonusMoneyValue : Int, DrawingValue : Int, EffectValue : String, CardName : String) {
-}
-object SetCards {
-
-}
