067c35b81d6f88ca60c7e10692ce1c0c1ed71a1c
Luis Nothvogel <L_noth@yahoo.de>
2019-04-25 18:48:19 +0200

    created desks

--- src/main/scala/de/htwg/se/dominion/model/GameStart.scala
+++ src/main/scala/de/htwg/se/dominion/model/GameStart.scala
@@ -3,25 +3,30 @@ package de.htwg.se.dominion.model
 import scala.collection.immutable.List
 import scala.util.Random
 
-case class GameStart() {
-  val basicDeck = List(BasicCards.copper, BasicCards.copper, BasicCards.copper, BasicCards.copper,
-    BasicCards.copper, BasicCards.copper, BasicCards.copper, BasicCards.mansion, BasicCards.mansion,
-    BasicCards.mansion)
+case class GameStart(a : BasicCards, b : BasicCards, c : BasicCards, d : BasicCards, e : BasicCards, f : BasicCards, g : BasicCards, h : BasicCards, i : BasicCards, j : BasicCards) {
 }
 
 object GameStart {
   def createDeck(pAmount:Int): Unit ={
     val random = new Random
 
-    val player1Deck,
-    val player2Deck,
-    val player3Deck,
-    val player4Deck,
-    val player5Deck,
+    val player1Deck = new GameStart(BasicCards.copper, BasicCards.copper, BasicCards.copper, BasicCards.copper,
+      BasicCards.copper, BasicCards.copper, BasicCards.copper, BasicCards.mansion, BasicCards.mansion,
+      BasicCards.mansion)
+    val player2Deck = new GameStart(BasicCards.copper, BasicCards.copper, BasicCards.copper, BasicCards.copper,
+      BasicCards.copper, BasicCards.copper, BasicCards.copper, BasicCards.mansion, BasicCards.mansion,
+      BasicCards.mansion)
+    val player3Deck = new GameStart(BasicCards.copper, BasicCards.copper, BasicCards.copper, BasicCards.copper,
+      BasicCards.copper, BasicCards.copper, BasicCards.copper, BasicCards.mansion, BasicCards.mansion,
+      BasicCards.mansion)
+    val player4Deck = new GameStart(BasicCards.copper, BasicCards.copper, BasicCards.copper, BasicCards.copper,
+      BasicCards.copper, BasicCards.copper, BasicCards.copper, BasicCards.mansion, BasicCards.mansion,
+      BasicCards.mansion)
+    val player5Deck = new GameStart(BasicCards.copper, BasicCards.copper, BasicCards.copper, BasicCards.copper,
+      BasicCards.copper, BasicCards.copper, BasicCards.copper, BasicCards.mansion, BasicCards.mansion,
+      BasicCards.mansion)
 
-
-
-
+    /*
     val emptyhand : List[BasicCards] = List()
     val emptyhand2 : List[BasicCards] = List()
     val deckhand: List[BasicCards] = BasicCards.copper :: BasicCards.copper :: BasicCards.copper :: BasicCards.copper :: BasicCards.copper :: BasicCards.copper :: BasicCards.copper :: BasicCards.mansion :: BasicCards.mansion :: BasicCards.mansion :: emptyhand
@@ -30,6 +35,6 @@ object GameStart {
       val y : List[BasicCards] = random.shuffle(deckhand)
       val x : List[BasicCards] = y.head :: y(1) :: y(2) :: y(3) :: y(4) :: emptyhand2
       println(x)
-    }
+    }*/
   }
 }
