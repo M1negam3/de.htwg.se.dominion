59e545e7821cccf43c76c4e0334ac2dee56624a2
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2019-04-26 20:34:22 +0200

    more hands

--- src/main/scala/de/htwg/se/dominion/model/Player.scala
+++ src/main/scala/de/htwg/se/dominion/model/Player.scala
@@ -3,3 +3,11 @@ package de.htwg.se.dominion.model
 case class Player(value : Int, name : String, deck : List[BasicCards], hand : List[BasicCards]) {
   override def toString: String = this.name
 }
+object Player {
+  val player1: List[BasicCards] = List()
+  val player2: List[BasicCards] = List()
+  val player3: List[BasicCards] = List()
+  val player4: List[BasicCards] = List()
+  val player5: List[BasicCards] = List()
+
+}
