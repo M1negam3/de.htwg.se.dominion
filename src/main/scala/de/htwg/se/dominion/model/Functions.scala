1035c17cc058f8e2b577fd9fa3a3c0c91d6ecbe8
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2019-05-07 18:38:08 +0200

    added Tests for Functions

--- src/main/scala/de/htwg/se/dominion/model/Functions.scala
+++ src/main/scala/de/htwg/se/dominion/model/Functions.scala
@@ -2,18 +2,18 @@ package de.htwg.se.dominion.model
 
 object Functions {
 
-  def HandMoney(l: List[BasicCards]): Integer = {
+  def handMoney(l: List[BasicCards]): Integer = {
     l.head.MoneyValue + l(1).MoneyValue + l(2).MoneyValue + l(3).MoneyValue +
       l(4).MoneyValue + l.head.BonusMoneyValue + l(1).BonusMoneyValue +
       l(2).BonusMoneyValue + l(3).BonusMoneyValue + l(4).BonusMoneyValue
   }
 
-  def HandActions(l: List[BasicCards]): Integer = {
+  def handActions(l: List[BasicCards]): Integer = {
     l.head.ActionValue + l(1).ActionValue + l(2).ActionValue + l(3).ActionValue +
       l(4).ActionValue
   }
 
-  def HandAdditionalBuys(l: List[BasicCards]): Integer = {
+  def handAdditionalBuys(l: List[BasicCards]): Integer = {
     l.head.BuyAdditionValue + l(1).BuyAdditionValue + l(2).BuyAdditionValue +
       l(3).BuyAdditionValue + l(4).BuyAdditionValue
   }
