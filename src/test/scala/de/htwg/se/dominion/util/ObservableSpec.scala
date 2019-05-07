2a572e8b93f389ef8d7d55c743156078cf794b61
Luis Nothvogel <L_noth@yahoo.de>
2019-05-07 18:18:29 +0200

    more tests

---
+++ src/test/scala/de/htwg/se/dominion/util/ObservableSpec.scala
@@ -0,0 +1,26 @@
+package de.htwg.se.dominion.util
+
+import java.security.KeyStore.TrustedCertificateEntry
+
+import org.scalatest.{Matchers, WordSpec}
+
+class ObservableSpec extends WordSpec with Matchers {
+  "An Observable" should {
+    val observable = new Observable
+    val observer: Observer = new Observer {
+      var updated: Boolean = false
+      override def update(): Unit = ()
+    }
+    "add an Observer" in {
+      observable.add(observer)
+      observable.subscribers should contain (observer)
+    }
+    "notify an Observer" in {
+      observable.notifyObservers()
+    }
+    "remove an Observer" in {
+      observable.remove(observer)
+      observable.subscribers should not contain observer
+    }
+  }
+}
