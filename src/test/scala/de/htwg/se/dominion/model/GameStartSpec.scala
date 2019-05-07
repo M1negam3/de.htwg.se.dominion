b85c8c7513107b493ae869d8c08190cbd7811b0f
Luis Nothvogel <L_noth@yahoo.de>
2019-05-07 11:30:16 +0200

    try to get travis to work, deleted useless tests

--- src/test/scala/de/htwg/se/dominion/model/GameStartSpec.scala
+++
@@ -1,8 +0,0 @@
-package de.htwg.se.dominion.model
-
-import org.scalatest._
-
-class GameStartSpec extends WordSpec with Matchers {
-
-
-}
--- src/test/scala/de/htwg/se/dominion/model/InputOutputSpec.scala
+++
@@ -1,7 +0,0 @@
-package de.htwg.se.dominion.model
-
-import org.scalatest._
-
-class InputOutputSpec extends WordSpec with Matchers {
-
-}
