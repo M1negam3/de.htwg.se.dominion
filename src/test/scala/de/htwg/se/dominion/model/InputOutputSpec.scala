85f5d03551db86075cdf5c573b5d5964995225bb
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2019-05-07 18:53:56 +0200

    added Tests for IO

--- src/test/scala/de/htwg/se/dominion/model/InputOutputSpec.scala
+++ src/test/scala/de/htwg/se/dominion/model/InputOutputSpec.scala
@@ -3,5 +3,17 @@ package de.htwg.se.dominion.model
 import org.scalatest._
 
 class InputOutputSpec extends WordSpec with Matchers {
+  val pCount = 1
 
+  "A InputOutput" should {
+    /*"have a getPlayerCount method" in {
+      InputOutput.getPlayerCount() should be ()
+    }*/
+    "have a deckcreation method" in {
+      InputOutput.deckCreation(pCount) should not be ("==> Deck " + pCount + " is created!")
+    }
+    /*"have a getPlayername method" in {
+      InputOutput.getPlayerName(pCount) should be (1)
+    }*/
+  }
 }
