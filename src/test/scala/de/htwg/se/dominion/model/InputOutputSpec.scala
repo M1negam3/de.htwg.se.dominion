9f32b57b46ea163d176c53fa19bd37de416c7185
Luis Nothvogel <L_noth@yahoo.de>
2019-05-07 19:06:42 +0200

    added I/O Tests

--- src/test/scala/de/htwg/se/dominion/model/InputOutputSpec.scala
+++ src/test/scala/de/htwg/se/dominion/model/InputOutputSpec.scala
@@ -3,17 +3,24 @@ package de.htwg.se.dominion.model
 import org.scalatest._
 
 class InputOutputSpec extends WordSpec with Matchers {
-  val pCount = 1
+  val pCount = 2
+  val l: List[BasicCards] = List(BasicCards.copper, BasicCards.copper, BasicCards.copper, BasicCards.copper,
+  BasicCards.copper)
 
   "A InputOutput" should {
-    /*"have a getPlayerCount method" in {
-      InputOutput.getPlayerCount() should be ()
-    }*/
-    "have a deckcreation method" in {
-      InputOutput.deckCreation(pCount) should not be ("==> Deck " + pCount + " is created!")
+
+    "have a getPlayerCount method" in {
     }
-    /*"have a getPlayername method" in {
-      InputOutput.getPlayerName(pCount) should be (1)
-    }*/
+
+    "have a getPlayername method" in {
+    }
+
+    "have a deckcreation method" in {
+      InputOutput.deckCreation(pCount) should startWith ("==> Deck ")
+    }
+
+    "have a HandCardCreation method" in {
+      InputOutput.HandCardCreation(l, pCount) should be("Player " + pCount + "`s Hand Cards are: Copper, Copper, Copper, Copper, Copper, ")
+    }
   }
 }
