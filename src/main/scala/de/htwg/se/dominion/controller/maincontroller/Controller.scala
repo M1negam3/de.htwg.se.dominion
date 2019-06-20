49a9fce08ac9e2185c173914c0d0df991c8f31c1
Luis Nothvogel <L_noth@yahoo.de>
2019-06-20 19:30:07 +0200

    testen

--- src/main/scala/de/htwg/se/dominion/controller/maincontroller/Controller.scala
+++ src/main/scala/de/htwg/se/dominion/controller/maincontroller/Controller.scala
@@ -23,13 +23,57 @@ class Controller(r: RoundManager) extends ControllerInterface {
     notifyObservers
   }
 
-  override def turn(): Unit = {
+  /*def newGame(): Unit = {
+    pCount = GameInit.getPlayerCount()
+    val names = GameInit.getPlayerName(pCount)
+    players = Player.createPlayer(pCount, names)
+    phaseString = Output.printPrep()
+    cPlayers = players
+    state = "turn"
+    notifyObservers
+  }*/
+
+  /*override def turn(): Unit = {
     roundmanager = roundmanager.playerTurn(roundmanager)
     undoManager.doStep(new turnCommand(roundmanager, roundmanager.idx,this))
     notifyObservers
     roundmanager = roundmanager.playerTurn(RoundManager(roundmanager.players, roundmanager.numberOfRounds, roundmanager.numberOfPlayers, roundmanager.names, roundmanager.score, roundmanager.idx + 1))
     roundmanager = roundmanager.roundNumber(roundmanager)
+  }*/
+
+  override def turn(): Unit = {
+  playerTurn = GameTurn.round(pCount, playerTurn)
+  phaseString = Output.printActionPhase() + Output.printTurn(playerTurn)
+  notifyObservers
+  cPlayers = GameTurn.actionPhase(cPlayers, playerTurn)
+  phaseString = Output.printBuyPhase()
+  notifyObservers
+  cPlayers = GameTurn.buyPhase(cPlayers, playerTurn)
+  phaseString = Output.printTurnEnd(playerTurn) + GameTurn.endCheck(GameTurn.end)
+  playerTurn += 1
+  if (GameTurn.end) {
+    state = "end"
   }
+  notifyObservers
+ }
+
+  /*override def turn(): Unit = {
+    roundmanager = roundmanager.playerTurn(roundmanager)
+    println("TEST " + roundmanager.players(roundmanager.idx).deck)
+    println("TEST " + roundmanager.players(roundmanager.idx).stacker)
+    println("TEST " + roundmanager.players(roundmanager.idx).hand)
+    roundmanager = roundmanager.actionPhase(roundmanager)
+    println("TEST1 " + roundmanager.players(roundmanager.idx).deck)
+    println("TEST1 " + roundmanager.players(roundmanager.idx).stacker)
+    println("TEST1 " + roundmanager.players(roundmanager.idx).hand)
+    roundmanager = roundmanager.buyPhase(roundmanager)
+    println("TEST2 " + roundmanager.players(roundmanager.idx).deck)
+    println("TEST2 " + roundmanager.players(roundmanager.idx).stacker)
+    println("TEST2 " + roundmanager.players(roundmanager.idx).hand)
+    roundmanager = roundmanager.playerTurn(RoundManager(roundmanager.players, roundmanager.numberOfRounds, roundmanager.numberOfPlayers, roundmanager.names, roundmanager.score, roundmanager.idx + 1))
+    roundmanager = roundmanager.roundNumber(roundmanager)
+    notifyObservers
+  }*/
 
   override def help(): Unit = {
     phaseString = Output.printRules()
