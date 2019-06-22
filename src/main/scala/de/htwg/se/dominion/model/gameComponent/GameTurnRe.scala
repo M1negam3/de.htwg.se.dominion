93bb0d181c248cdbd06ce0815d62c6592935def2
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2019-06-22 22:58:30 +0200

    throwing everything around

--- src/main/scala/de/htwg/se/dominion/model/gameComponent/GameTurnRe.scala
+++ src/main/scala/de/htwg/se/dominion/model/gameComponent/GameTurnRe.scala
@@ -1,5 +1,42 @@
 package de.htwg.se.dominion.model.gameComponent
 
-class GameTurnRe {
+import de.htwg.se.dominion.model.playerComponent.Player
+
+object GameTurnRe {
+
+
+
+
+  def actionPhase(list: List[Player], idx: Int): List[Player] = {
+    var l = list
+    var index = idx
+    for (f <- 0 until 5) {
+      if (l(index).hand(f).Type.equals("Action") && !check) {
+        l(index).actions = 1
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
 
 }
