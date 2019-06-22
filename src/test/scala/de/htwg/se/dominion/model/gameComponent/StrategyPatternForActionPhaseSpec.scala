95127a1ad364a2d15963049fdb9e0badbaffb36b
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2019-06-22 15:18:29 +0200

    added more tests

--- src/test/scala/de/htwg/se/dominion/model/gameComponent/StrategyPatternForActionPhaseSpec.scala
+++ src/test/scala/de/htwg/se/dominion/model/gameComponent/StrategyPatternForActionPhaseSpec.scala
@@ -9,22 +9,22 @@ class StrategyPatternForActionPhaseSpec extends WordSpec with Matchers{
 
   var Luca = new Player("Luca",0,Cards.startDeck,Cards.stacker,Cards.hand)
   var Luis = new Player("Luis",0,Cards.startDeck,Cards.stacker,Cards.hand)
-  var list: List[Player] = List(Luca,Luis)
+  var l: List[Player] = List(Luca,Luis)
+  var i = 0
+  var playingCards: List[Cards] = List(Cards.cellar,Cards.mine,Cards.remodel,Cards.workshop,Cards.merchant)
   "A StrategyPatternForAction" should {
-    "have a case Cellar" in {
-
+    "have a strategy" in {
+      var strategy = playingCards.head.CardName match {
+        case "Cellar" => GameTurn.cellar(l, i) should be (l)
+        case "Mine" => GameTurn.mine(l, i) should be (l)
+        case "Remodel" => GameTurn.remodel(l, i) should be (l)
+        case "Workshop" => GameTurn.workshop(l, i) should be (l)
+        case "Merchant" =>
+          GameTurn.money = GameTurn.money + GameTurn.merchant(l, i)
+          l should be (GameTurn.l)
+        case _ => GameTurn.l should be (GameTurn.l)
+      }
     }
-    "have a case Mine" in {
 
-    }
-    "have a case  Remodel" in {
-
-    }
-    "have a case Workshop " in {
-
-    }
-    "have a case Merchant" in {
-
-    }
   }
 }
