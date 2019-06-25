29987043c7def31685ab1b25c8b023413e96d83e
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2019-06-25 14:03:35 +0200

    Added GameEndSpec

--- src/test/scala/de/htwg/de/dominion/model/gameComponent/GameEndSpec.scala
+++ src/test/scala/de/htwg/de/dominion/model/gameComponent/GameEndSpec.scala
@@ -1,5 +1,20 @@
-package de.htwg.de.dominion.model.gameComponent
+package de.htwg.se.dominion.model.gameComponent
 
-class GameEndSpec {
+import de.htwg.se.dominion.model.deckComponent.cardComponent._
+import de.htwg.se.dominion.model.playerComponent.Player
+import org.scalatest.{Matchers, WordSpec}
+class GameEndSpec extends WordSpec with Matchers{
+  var stacker: List[Cards] =List(Cards.copper,Cards.copper)
+  var Luca = new Player("Luca",0,Cards.startDeck,stacker,Cards.hand)
+  var Luis = new Player("Luis",0,Cards.startDeck,stacker,Cards.hand)
+  var list: List[Player] = List(Luca,Luis)
+  "A GameEnd" should {
+    "have a end method" in {
+      GameEnd.end(list).head.stacker.length should  be (0)
 
+    }
+    "have a score method" ignore {
+      GameEnd.score(list).head should be ("Luis",0)
+    }
+  }
 }
