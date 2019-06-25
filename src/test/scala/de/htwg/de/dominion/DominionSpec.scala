068db7accb883764ab733a60da96611b4e78659b
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2019-06-25 13:49:47 +0200

    added all testSpecs

---
+++ src/test/scala/de/htwg/de/dominion/DominionSpec.scala
@@ -0,0 +1,12 @@
+package de.htwg.se.dominion
+
+import org.scalatest._
+
+class DominionSpec extends WordSpec with Matchers {
+
+  "The Dominion main class" should {
+    "accept text input as argument without readline loop, to test it from command line" in {
+      Dominion.main(Array[String]("s"))
+    }
+  }
+}
