a9bb042cd120a804254977cf3680822499476c1a
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2019-06-25 19:49:28 +0200

    startet StrategyPatternForActionPhase/ for hand is done

--- src/test/scala/de/htwg/de/dominion/model/gameComponent/StrategyPatternForActionPhaseSpec.scala
+++ src/test/scala/de/htwg/de/dominion/model/gameComponent/StrategyPatternForActionPhaseSpec.scala
@@ -3,14 +3,30 @@ package de.htwg.se.dominion.model.gameComponent
 import de.htwg.se.dominion.model.deckComponent.cardComponent._
 import de.htwg.se.dominion.model.gameComponent._
 import de.htwg.se.dominion.model.playerComponent.Player
+import de.htwg.se.dominion.model.gameComponent.StrategyPatternForActionPhase
 import org.scalatest.{Matchers, WordSpec}
 
 class StrategyPatternForActionPhaseSpec extends WordSpec with Matchers{
 
-
+  var hand: List[Cards] = List(Cards.copper,Cards.merchant,Cards.silver,Cards.copper,Cards.copper)
   var Luca = new Player("Luca",0,Cards.startDeck,Cards.stacker,Cards.hand)
+  var Luca1 = new Player("Luca",0,Cards.startDeck,Cards.stacker,Cards.hand,Nil,1,0,22)
+  var Luca2 = new Player("Luca",0,Cards.startDeck,Cards.stacker,Cards.hand,Nil,1,0,20,2)
+  var Luca3 = new Player("Luca",0,Cards.startDeck,Cards.stacker,Cards.hand,Nil,1,0,21)
+  var Luca4 = new Player("Luca",0,Cards.startDeck,Cards.stacker,Cards.hand,Nil,1,0,40)
+
+
+  var Luca5 = new Player("Luca",0,Cards.startDeck,Cards.stacker,hand,Nil,1,0)
+
   var Luis = new Player("Luis",0,Cards.startDeck,Cards.stacker,Cards.hand)
   var l: List[Player] = List(Luca,Luis)
+  var l1: List[Player] = List(Luca1,Luis)
+  var l2: List[Player] = List(Luca2,Luis)
+  var l3: List[Player] = List(Luca3,Luis)
+  var l4: List[Player] = List(Luca4,Luis)
+  var l5: List[Player] = List(Luca5,Luis)
+  var l6: List[Player] = List(Luca2,Luis)
+
   var i = 0
   var playingCards: List[Cards] = List(Cards.cellar,Cards.mine,Cards.remodel,Cards.workshop,Cards.merchant)
   "A StrategyPatternForAction" should {
@@ -22,6 +38,28 @@ class StrategyPatternForActionPhaseSpec extends WordSpec with Matchers{
         //case "Workshop" => StrategyPatternForActionPhase.workshop(l, i) should be (l)
         //case "Merchant" =>
         case _ => GameTurn.l should be (GameTurn.l)
+      }
+      "have a cellar method" in {
+
+      }
+      "have a cellar2 method" in {
+
+      }
+      "have a mine method" in {
+        StrategyPatternForActionPhase.merchant(l,0) should be (l)
+      }
+      "have a remodel method" in {
+        StrategyPatternForActionPhase.remodel(l,0,0) should be (l)
+      }
+      "have a remodel2 method" in {
+        StrategyPatternForActionPhase.remodel2(l,0,0) should be (l)
+      }
+      "have a workshop method " in {
+        StrategyPatternForActionPhase.workshop(l,0,0) should be (l)
+      }
+      "have a merchant method" in {
+        StrategyPatternForActionPhase.merchant(l,0) should be (l1)
+        StrategyPatternForActionPhase.merchant(l5,0) should be (l6)
       }
     }
 
--- src/test/scala/de/htwg/de/dominion/model/gameComponent/StrategyPatternForHandSpec.scala
+++ src/test/scala/de/htwg/de/dominion/model/gameComponent/StrategyPatternForHandSpec.scala
@@ -1,5 +1,26 @@
 package de.htwg.de.dominion.model.gameComponent
 
-class StrategyPatternForHandSpec {
+import de.htwg.se.dominion.model.deckComponent.cardComponent.Cards
+import de.htwg.se.dominion.model.gameComponent.{GameTurn, StrategyPatternForHand}
+import org.scalatest.{Matchers, WordSpec}
+import de.htwg.se.dominion.model.gameComponent.StrategyPatternForHand._
+import de.htwg.se.dominion.model.playerComponent.Player
 
+import scala.collection.mutable.ListBuffer
+
+class StrategyPatternForHandSpec extends  WordSpec with Matchers{
+  var l = new ListBuffer[Cards]
+
+  "A stragey " should {
+    "have a strategy " in {
+      StrategyPatternForHand.strategy should not be (5)
+      StrategyPatternForHand.strategy0(Player.copiedPlayer, Player.copyList) should be (l)
+      StrategyPatternForHand.strategy1(Player.copiedPlayer, Player.copyList) should be (l)
+      StrategyPatternForHand.strategy2(Player.copiedPlayer, Player.copyList) should be (l)
+      StrategyPatternForHand.strategy3(Player.copiedPlayer, Player.copyList) should be (l)
+      StrategyPatternForHand.strategy4(Player.copiedPlayer, Player.copyList) should be (l)
+
+    }
+
+  }
 }
