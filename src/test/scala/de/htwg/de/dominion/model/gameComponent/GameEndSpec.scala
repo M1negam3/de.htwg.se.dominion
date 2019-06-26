2e5637de2ea3d743a941a1c27622bc187867d2dc
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2019-06-26 16:25:32 +0200

    Finished gameEnd

--- src/test/scala/de/htwg/de/dominion/model/gameComponent/GameEndSpec.scala
+++ src/test/scala/de/htwg/de/dominion/model/gameComponent/GameEndSpec.scala
@@ -5,15 +5,16 @@ import de.htwg.se.dominion.model.playerComponent.Player
 import org.scalatest.{Matchers, WordSpec}
 class GameEndSpec extends WordSpec with Matchers{
   var deck: List[Cards] =List(Cards.copper,Cards.copper,Cards.copper,Cards.copper,Cards.copper,Cards.copper,Cards.copper,Cards.estate,Cards.estate,Cards.gardens)
-  var Luca = new Player("Luca",0,deck,Cards.stacker,Cards.hand)
-  var Luis = new Player("Luis",0,Cards.startDeck,Cards.stacker,Cards.hand)
+  var stacker: List[Cards] = List(Cards.copper,Cards.copper)
+  var Luca = new Player("Luca",0,deck,stacker,Cards.hand)
+  var Luis = new Player("Luis",0,Cards.startDeck,stacker,Cards.hand)
   var list: List[Player] = List(Luca,Luis)
   var finished: List[(Int,String)] = List((4,"Luca"),(3,"Luis"))
   "A GameEnd" should {
     "have a end method" in {
       GameEnd.end(list).head.stacker.length should  be (0)
     }
-    "have a score method" ignore {
+    "have a score method" in {
       GameEnd.score(list) should  be (finished)
     }
   }
