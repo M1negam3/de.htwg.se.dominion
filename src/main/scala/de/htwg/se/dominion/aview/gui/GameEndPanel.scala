a6c59deb5e2944bf35fe94a95fd695aee3a14835
Luis Nothvogel <L_noth@yahoo.de>
2019-06-26 13:31:34 +0200

    added PlayingPanel & GameEndPanel

---
+++ src/main/scala/de/htwg/se/dominion/aview/gui/GameEndPanel.scala
@@ -0,0 +1,9 @@
+package de.htwg.se.dominion.aview.gui
+
+import de.htwg.se.dominion.controller.maincontroller.Controller
+import scala.swing.{BoxPanel, Orientation}
+
+
+class GameEndPanel(controller: Controller) extends BoxPanel(Orientation.Vertical) {
+  
+}
---
+++ src/main/scala/de/htwg/se/dominion/aview/gui/PlayingPanel.scala
@@ -0,0 +1,8 @@
+package de.htwg.se.dominion.aview.gui
+
+import de.htwg.se.dominion.controller.maincontroller.Controller
+import scala.swing._
+
+class PlayingPanel(controller: Controller) extends BoxPanel(Orientation.Vertical) {
+
+}
