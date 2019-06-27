e1c4b1a92aca741c0818e3d4d236a4558a967046
Luca Stratmann <luca.stratmann@gmx-topmail.de>
2019-06-27 23:06:46 +0200

    found teh shitcode

--- src/test/scala/de/htwg/de/dominion/model/gameComponent/GameEndSpec.scala
+++ src/test/scala/de/htwg/de/dominion/model/gameComponent/GameEndSpec.scala
@@ -5,9 +5,10 @@ import de.htwg.se.dominion.model.playerComponent.Player
 import org.scalatest.{Matchers, WordSpec}
 class GameEndSpec extends WordSpec with Matchers{
   var deck: List[Cards] =List(Cards.copper,Cards.copper,Cards.copper,Cards.copper,Cards.copper,Cards.copper,Cards.copper,Cards.estate,Cards.estate,Cards.gardens)
+  var deck2: List[Cards] =List(Cards.copper,Cards.copper,Cards.copper,Cards.copper,Cards.copper,Cards.copper,Cards.copper,Cards.estate,Cards.estate,Cards.estate)
   var stacker: List[Cards] = List(Cards.copper,Cards.copper)
   var Luca = new Player("Luca",0,deck,stacker,Cards.hand)
-  var Luis = new Player("Luis",0,Cards.startDeck,stacker,Cards.hand)
+  var Luis = new Player("Luis",0,deck2,stacker,Cards.hand)
   var list: List[Player] = List(Luca,Luis)
   var finished: List[(Int,String)] = List((3,"Luca"),(3,"Luis"))
   "A GameEnd" should {
